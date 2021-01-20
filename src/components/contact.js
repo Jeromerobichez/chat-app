e1e9e47a0be4c0c9ac1a47bfff76bbc30a54e27b
Jeromerobichez <jerome.robichez@gmail.com>
2021-01-20 11:29:21 +0100

    update online button

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React from "react";
 import "./components.css";
 
 function Contact() {
-    return (
-      <div className="Contact">
-          <img className="avatar" src="https://randomuser.me/api/portraits/men/11.jpg" alt='Peter Shelton'/>
-          <div className="ContactInfos">
-          <div className='name'>Peter Shelton</div>
-          <div className='status'>
-            <div className='status-indicator'>
-
-            </div>
-          </div>
-          </div>
-          
-       
+  return (
+    <div className="Contact">
+      <img
+        className="avatar"
+        src="https://randomuser.me/api/portraits/men/11.jpg"
+        alt="Peter Shelton"
+      />
+      <div className="ContactInfos">
+        <div className="name">Peter Shelton</div>
+        <div className="status">
+          <div className="status-online"> 
+          <div className="status-text">online</div>
+        </div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Contact;
