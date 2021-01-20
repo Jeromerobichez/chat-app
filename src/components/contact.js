569e9fb8dbfe34c091ffdab6eaadcabceb942870
Jeromerobichez <jerome.robichez@gmail.com>
2021-01-20 11:22:17 +0100

    chat-app1

---
+++ src/components/contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./components.css";
+
+function Contact() {
+    return (
+      <div className="Contact">
+          <img className="avatar" src="https://randomuser.me/api/portraits/men/11.jpg" alt='Peter Shelton'/>
+          <div className="ContactInfos">
+          <div className='name'>Peter Shelton</div>
+          <div className='status'>
+            <div className='status-indicator'>
+
+            </div>
+          </div>
+          </div>
+          
+       
+      </div>
+    );
+  }
+
+export default Contact;
