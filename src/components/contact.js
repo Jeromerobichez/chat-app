372a1f58cac6d083bd74a7d9a0ac4b0bd04498d8
Jeromerobichez <jerome.robichez@gmail.com>
2021-01-27 12:41:04 +0100

    adding the condition for online offline

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,27 +1,42 @@
 import React from "react";
 import "./components.css";
 
-
-function Contact(props) {
-  return (
-    <div className="Contact">
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: false
+    }
+  }
+  
+  render(){
+    return (
+      <div className="Contact">
       <img
         className="avatar"
-        src={props.avatar}
-        alt={props.name}
+        src={this.props.avatar}
+        alt={this.props.name}
       />
-
-      
       <div className="status">
-      <h4 className="name">{props.name}</h4>
-          <span className="status-online">
+      <h4 className="name">{this.props.name}</h4>
+      <div className={this.state.online ? 'isOnline' : 'isOffline'}
+      onClick={(e) => {
+        const newStatus = !this.state.online
+      this.setState({online: newStatus})
+      console.log(newStatus)
+     }
+    }
+      >
+          <span className={this.state.online ? 'status-online' : 'status-offline'}>
           </span>
           
-        <h5 className="status-text">Online</h5>
+        <h5 className="status-text">{this.state.online ? 'Online' : 'Offline'}</h5>
+        </div>
       </div>
     </div>
-    
-  );
+    )
+  }
 }
+
 
 export default Contact;
